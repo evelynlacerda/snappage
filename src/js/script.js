2e06c8817c0cdd95b387e93d4d230676c7224242
Evelyn Lacerda <evelyndslacerda@gmail.com>
2023-11-06 02:57:21 -0300

    feat: create logic for mobile menu and header on mobile

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,55 @@ function handleArrowClick(arrowId, dropId) {
 
 handleArrowClick('arrowFeature', 'dropFeature');
 handleArrowClick('arrowCompany', 'dropCompany');
+
+// ----------------------------------------------------------------
+
+function replaceImage() {
+    const imageHero = document.querySelector('.img-hero');
+    
+    if (window.innerWidth < 992) {
+        imageHero.src = './src/assets/img/image-hero-mobile.png'
+    } else {
+        return;
+    }
+}
+
+window.addEventListener('load', replaceImage);
+window.addEventListener('resize', replaceImage);
+
+// ----------------------------------------------------------------
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileBoxMenu = document.querySelector('.box-menu');
+const menu = document.querySelector('.menu');
+
+function toggleMobileMenu() {
+
+    const isMenuChecked = mobileMenu.checked;
+
+    mobileBoxMenu.style.display = isMenuChecked ? 'flex' : 'none';
+
+    const animateClass = 'animate__animated';
+    const fadeInClass = 'animate__fadeIn';
+    const slideInRightClass = 'animate__fadeInRight';
+
+    if (isMenuChecked) {
+        mobileBoxMenu.classList.add(animateClass, fadeInClass);
+        menu.classList.add(animateClass, slideInRightClass);
+    } else {
+        mobileBoxMenu.classList.remove(animateClass, fadeInClass);
+        menu.classList.remove(animateClass, slideInRightClass);
+    }
+}
+
+mobileMenu.addEventListener('click', toggleMobileMenu);
+
+// ----------------------------------------------------------------
+
+const header = document.querySelector('.header');
+
+function toggleHeaderVisibility() {
+    header.style.display = window.scrollY > 80 ? 'none' : 'flex';
+}
+
+window.addEventListener('scroll', toggleHeaderVisibility);
